dda57a8cebd0d9242d6d5f7e303c933cc51136c1
Hein Min Thant <heinminthant1999@gmail.com>
2023-03-02 17:43:51 +0630

    add: freecodecamp url shortener microservice

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("node:dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const urls = [];
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body.url;
+
+  const regex = /^(ftp|http|https):\/\/[^ "]+$/;
+
+  if (!regex.test(url)) {
+    return res.json({ error: "Invalid URL" });
+  }
+
+  urls.push(url);
+
+  res.json({
+    original_url: url,
+    short_url: urls.length,
+  });
+});
+
+app.get("/api/shorturl/:url", function (req, res) {
+  const url = Number(req.params.url);
+
+  if (urls[url - 1]) {
+    res.redirect(urls[url - 1]);
+  }
+
+  return res.json({
+    error: "Invalid short url",
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
